4b37e8fed415daff1aeb9118a7fec3510533b60b
Leonardo Grava de Mendonça <leonardo.grava@outlook.com>
2026-02-04 02:14:30 -0300

    limpeza e ajustes finais

--- avaliacoes/urls.py
+++ avaliacoes/urls.py
@@ -1,23 +1,15 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login',views.index, name='index'),
-
-    path('menu',views.menu, name='menu'),
-
-    #logoff
-    path('sair',views.sair,name='sair'),
-
-    #logout
-    path('logoff',views.logoff,name='logoff'),
-    path('responsaveis',views.responsaveis,name='responsaveis'),
-    path('turmas',views.turmas,name='turmas'),
-    path('avaliacoes',views.avaliacoes,name='avaliacoes'),
-    path('professores',views.professores,name='professores'),
-    path('alunos',views.alunos,name='alunos'),
-    path('relatorios',views.relatorios,name='relatorios'),
+    path('index', views.index, name='index'),
+    path('menu', views.menu, name='menu'),
+    path('responsaveis', views.responsaveis, name='responsaveis'),
+    path('turmas', views.turmas, name='turmas'),
+    path('avaliacoes', views.avaliacoes, name='avaliacoes'),
+    path('professores', views.professores, name='professores'),
+    path('alunos', views.alunos, name='alunos'),
+    path('relatorios', views.relatorios, name='relatorios'),
     path('relatorios/data', views.relatorios_data, name='relatorios_data'),
     path('relatorios/pdf', views.relatorios_pdf, name='relatorios_pdf'),
-    ]
+]
